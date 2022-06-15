1e98722126c3e1b65bafb47014309ef030bb65a6
aprilia anggun sari rahmawati <92620594+aprilianggun@users.noreply.github.com>
2022-06-15 19:47:40 +0700

    Add files via upload

---
+++ project uap petruk.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+#include<windows.h>
+using namespace std;
+
+list<int>Kereta;
+list<int>::iterator it;
+int temp;
+
+
+void Push(){
+	cout<<"Masukan Nomor Antrian: ";
+	cin>>temp;
+	Kereta.push_back(temp);
+	
+}
+
+void pop(){
+	Kereta.pop_front();
+}
+
+void clear(){
+	Kereta.clear();
+}
+void print(){
+	cout<<"\tantrian: "<<endl;
+	cout<<"==========================\n";
+    for(it = Kereta.begin(); it != Kereta.end();it++){
+	    Sleep(500);
+        cout<<"||antrian nomor >> "<<*it<<" <<||\n";
+    }
+    cout<<"==========================\n\n";
+}
+
+int main(){
+	
+	int p;
+	
+	do{
+		
+		cout<<"~~SELAMAT DATANG DI STASIUN TANAH ABANG~~\n";
+		cout<<"SILAHKAN MENGANTRI DENGAN TERTIB!\n\n";
+		system("color 71");
+		
+		print();
+		cout<<"~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout<<"\tMenu"<<endl;
+		cout<<"~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		cout<<"1. Tambah Antrian"<<endl;
+		cout<<"2. Panggil Antrian"<<endl;
+		cout<<"3. Kosongkan Antrian"<<endl;
+		cout<<"4. Exit"<<endl;
+		cout<<"~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+		
+		cout<<"\nPilih Menu: ";
+		cin>>p;
+		
+		system("cls");
+		
+		if(p==1){
+			Push();
+		}else if(p==2){
+			pop();
+			cout<<"ANTRIAN PALING DEPAN TELAH DIPANGGIL~~\n";
+		}else if(p==3){
+			clear();
+			cout<<"ANTRIAN PADA STASIUN TELAH DIBERSIHKAN!\n";
+		}else if(p==4){
+			cout<<"~TERIMAKASIH ATAS KUNJUNGANYA~\n";
+		}else{
+			cout<<"TIDAK TERDETEKSI DALAM SISTEM STASIUN MANAPUN!\n";
+		}
+	}
+	while(p != 4);
+	
+	return 0;
+}
